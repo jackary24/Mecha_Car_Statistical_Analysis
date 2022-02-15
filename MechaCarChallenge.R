8e2ddf7363eecec164768192c4bdf73ebee6a510
jackary24 <80048254+jackary24@users.noreply.github.com>
2022-02-15 17:45:11 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,19 @@ MechaCar_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors =
 library(dplyr)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
+
+Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI))
+
+Lots_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
